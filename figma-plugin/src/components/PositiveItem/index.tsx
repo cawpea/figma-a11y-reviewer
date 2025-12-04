00ea9d0b14460f9f288056875f91374c6ac3c4d3
m.osumi <m.osumi@goodpatch.com>
2025-12-05 00:05:07 +0900

    :art: 文字サイズ調整

--- figma-plugin/src/components/PositiveItem/index.tsx
+++ figma-plugin/src/components/PositiveItem/index.tsx
@@ -7,7 +7,7 @@ interface PositiveItemProps {
 
 export default function PositiveItem({ text }: PositiveItemProps) {
   return (
-    <li className="text-green-700 text-xs relative pl-[1.1rem]">
+    <li className="text-green-700 text-[11px] relative pl-[1.1rem]">
       <IconCheck16 className="absolute left-0 inline-block align-middle" />
       <p className="inline-block">{text}</p>
     </li>
